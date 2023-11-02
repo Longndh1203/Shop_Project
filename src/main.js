d2b4f1500c447759309b2453eccbd4c59493a830
Long <longndh@smartosc.com>
2023-11-02 14:22:02 +0700

    config libraby

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import { BootstrapVue } from "bootstrap-vue";
+import Axios from "axios";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Axios);
+Vue.use(BootstrapVue);
 
 new Vue({
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
